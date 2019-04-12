19c894a8fb793d44bc4908445d177bab380485d0
Jimmy Chen Chen <jchenche@Computer.local>
2019-04-11 23:05:11 -0700

    did some clean up

---
+++ apps/File.h
@@ -0,0 +1,35 @@
+#ifndef __File_h__
+#define __File_h__
+
+// Internal library
+void  readBlock(FILE* disk, int blockNum, char* buffer);
+void  writeBlock(FILE* disk, int blockNum, char* data);
+short find_bit_one(int c);
+short find_available_block(FILE* disk, int data_type);
+void  deallocate_block(FILE* disk, short blockNum);
+int   writeToFile(FILE* disk, char* data, short inode_id, int size);
+int   readFromFile(FILE* disk, char* data, short inode_id, int size);
+int   get_file_size(FILE* disk, short inode_id);
+short find_inode(FILE* disk, char* name, short directory_inode);
+int   is_flat_file(FILE* disk, short inode_id);
+short walk_path(FILE* disk, char* _path);
+short find_file_inode(FILE* disk, char* name, char* path);
+short find_file_inode_with_parent(FILE* disk, char* name, char* path, short* parent_dir_inode);
+int   name_collision(FILE* disk, short directory_inode, char* name);
+void  file_system_check(FILE* disk);
+short createFile(FILE* disk, char* name, int type, char* path);
+short deleteFile(FILE* disk, char* name, int type, char* path);
+
+// The API
+short Read(char* name, char* buffer, int size, char* path);
+short Write(char* name, char* data, int size, char* path);
+short Rmdir(char* name, char* path);
+short Rm(char* name, char* path);
+short Mkdir(char* name, char* path);
+short Touch(char* name, char* path);
+void  InitLLFS();
+
+// Testing functions
+int   get_size(char* name, char* path);
+
+#endif
